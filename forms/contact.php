a33af520bdc545b100e0d695d8c6f58d2f11efda
Okonkwo Chinonso Emmanuel <milestonetech96@gmail.com>
2021-08-19 10:01:01 +0100

    ...2

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,28 @@
 <?php
-require_once '../db/conn.php';
+
+$state = 'production';
+
+if ($state === 'production') {
+    // $connectionStringHerokuEnv = 'mysql://ba8a0ffe44363d:330cb2d3@us-cdbr-east-04.cleardb.com/heroku_e00f674c3f63fbb?reconnect=true';
+    // $parsed = parse_url($connectionStringHerokuEnv);
+    // $dbname = ltrim($parsed['path']. '/'); 
+    // $connection = new PDO("{$parsed['scheme']}:host={$parsed};$dbname={$dbname};charset=utf8mb4", $parsed['user'], $parsed['pass'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    $url = parse_url("mysql://ba8a0ffe44363d:330cb2d3@us-cdbr-east-04.cleardb.com/heroku_e00f674c3f63fbb?reconnect=true");
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    // $connection = new mysqli($server, $username, $password, $db);
+    $connection = new PDO("mysql:host=$server;dbname=$db", "$username", "$password");
+	
+}else{
+
+    $connection = new PDO("mysql:host=localhost;dbname=hng-resume", "root", "");
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 
 
 
